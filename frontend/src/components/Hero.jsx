c78d9f013bdd98164332c274c86c8448a1cfb91f
Donzo <donzosd63@gmail.com>
2026-09-05 18:42:41 +0000

    Fix React runtime crash due to missing icon imports

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFacebook, FaWhatsapp } from 'react-icons/fa';
 
 import bgImage from '../assets/profile.jpg';
 
